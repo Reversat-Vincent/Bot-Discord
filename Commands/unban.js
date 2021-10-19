b1ac2bbff00a94097fdf40d2af43b9af5c1de0ac
REVERSAT VINCENT p1905074 <vincent.reversat@etu.univ-lyon1.fr>
2021-10-19 15:48:37 +0200

    unban work

--- Commands/unban.js
+++ Commands/unban.js
@@ -4,17 +4,21 @@ module.exports = {
     execute(msg, args) {
         let err = "";
         if (msg.member.hasPermission("BAN_MEMBERS")) {
-            let target = msg.mentions.members.first();
-            if (target) {
-                if (!(target.id === msg.author.id)) {
-                    target.unban(target.id);
-                    msg.reply("<@!"+target.id+"> was unban");
+            msg.guild.fetchBans().then(bans => {
+                let target = bans.find( u => {
+                    return '@' + u.user.username === args[1];
+                });
+                if (target != undefined) {
+                    if (!(target.user.id === msg.author.id)) {
+                        msg.guild.members.unban(target.user.id);
+                        msg.reply("<@!"+target.user.id+"> was unban");
+                    } else {
+                        err = err.concat("<@user> ne doit pas vous désigner");
+                    }
                 } else {
-                    err = err.concat("<@user> ne doit pas vous désigner");
+                    err = err.concat("<@user> est manquant");
                 }
-            } else {
-                err = err.concat("<@user> est manquant");
-            }
+            });
         } else {
             err = err.concat("Vous n'avez pas les permissions nécessaire");
         }
